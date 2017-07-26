5a6a90aa17adfb0d5913c02f065aaf4285ad0522
neerajkaushik95 <neeraj.kaushik95@gmail.com>
2017-07-26 18:21:30 +0530

    add packages

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-README.txt
-
 ## Synopsis
 
-A Sample Project For managinf Albumz built using [ExpressJS](http://expressjs.com/) and [NodeJS](http://nodejs.org).
+A Sample Project For managing Albumz built using [ExpressJS](http://expressjs.com/) and [NodeJS](http://nodejs.org).
 
 ## Installation
 
 1. Download and extract the files
 2. Enter directory from Node command/terminal
 3. Issue command: ```npm install```
-4. Start with: ```npm start
+4. Start with: npm start
--- app.js
+++ app.js
@@ -4,11 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var passport = require('passport');
+var localStratergy = require('passport-local').Strategy;
+require('dotenv').config();
+const session = require('express-session');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+ mongoose.connect(process.env.MONGODBHOST, {
+    useMongoClient: true,
+    /* other options */
+});
+
+//set App Port
+app.set('port', process.env.PORT);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +37,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressValidator());
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSIONSECRET
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use('/', index);
 app.use('/users', users);
@@ -35,6 +64,10 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.user = req.user || null;
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -44,3 +77,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
